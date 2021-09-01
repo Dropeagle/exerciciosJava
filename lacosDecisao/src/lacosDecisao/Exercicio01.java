a0a42df227826cce77950742aeb4b25ed85e83a1
Drope <papagodao.gota@gmail.com>
2021-09-01 20:33:10 -0300

    Laços de decisão

---
+++ lacosDecisao/src/lacosDecisao/Exercicio01.java
@@ -0,0 +1,29 @@
+package lacosDecisao;
+import java.util.Scanner;
+
+public class Exercicio01 {
+	
+	public static void main (String[]args) {
+		
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite três números: ");
+		
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+		num3 = scan.nextInt();
+		scan.close();
+		
+		if(num1 > num2 && num1 > num3)
+			System.out.println("O primeiro numero digitado é o maior, numero digitado = " + num1);
+		
+		if(num2 > num1 && num2 > num3)
+			System.out.println("O segundo numero digitado é o maior, numero digitado = " + num2 );
+		
+		if(num3 > num1 && num3 > num2)
+			System.out.println("O terceiro numero digitado é o maior, numero digitado = " + num3);
+	}
+}
--- lacosDecisao/src/lacosDecisao/Exercicio03.java
+++ lacosDecisao/src/lacosDecisao/Exercicio03.java
@@ -1,26 +1,25 @@
 package lacosDecisao;
-
 import java.util.Scanner;
-public class Exercicio03 {
+
+public class Exercicio03{
+	
 	public static void main (String[] args) {
 		int idade = 0;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Digite sua idade: ");
-		
 		idade = scan.nextInt();
 		scan.close();
 		
-			if(idade >= 10 && idade <= 14)
-				System.out.println("Infantil");
+		if(idade >= 10 && idade <= 14)
+			System.out.println("Infantil");
+
+			System.out.println("Juvenil");
 			
-			if(idade >= 15 && idade <= 17)
-				System.out.println("Juvenil");
-			
-			if(idade >= 18 && idade <= 25)
-				System.out.println("Adulto");
-			
-			if(idade < 10 || idade > 25) 
-				System.out.println("Entrada invalida");
+		if(idade >= 18 && idade <= 25)
+			System.out.println("Adulto");
+		
+		if(idade < 10 || idade > 25) 
+			System.out.println("Entrada invalida");
 	}
 }
