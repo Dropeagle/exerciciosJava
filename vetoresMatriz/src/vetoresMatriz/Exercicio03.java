49968bf42a47eb30b31e68d89e1c4049beb72100
Drope <papagodao.gota@gmail.com>
2021-09-03 15:20:50 -0300

    vetoresMatriz

---
+++ vetoresMatriz/src/vetoresMatriz/Exercicio03.java
@@ -0,0 +1,30 @@
+package vetoresMatriz;
+
+import java.util.Random;
+
+import java.util.Arrays;
+
+public class Exercicio03 {	
+	public static void main(String[] args) {	
+	   int i, soma = 0, subida = 0, jogadas[] = new int[10];
+	   double media = 0;
+	   Random n = new Random();
+
+	   for(i = 0; i < 10; ++i) {
+		   jogadas[i] = n.nextInt(6) + 1;
+		   soma += jogadas[i];
+
+		  if(jogadas[i] > media) {
+			  subida = jogadas[i];
+			  soma = 0;
+}
+	         
+	  }for(i=0;i<10;++i) {
+	      if(jogadas[i] > media)	    	   	
+	          ++soma;
+
+	   }
+	  System.out.println("Sorteados: " + Arrays.toString(jogadas));
+	  System.out.println("%d Números são maiores que média aritmética dos 10 lançamentos\n%d porcento das jogadas foram 6\n" + soma + (subida * 100)/10);
+	}
+}
