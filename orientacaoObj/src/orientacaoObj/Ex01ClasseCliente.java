b5734d4dd846d86c2b8c50625625e38202372076
Drope <papagodao.gota@gmail.com>
2021-09-09 19:36:43 -0300

    Orientação a objetos

--- orientacaoObj/src/orientacaoObj/Ex01ClasseCliente.java
+++ orientacaoObj/src/orientacaoObj/Ex01ClasseCliente.java
@@ -1,5 +1,33 @@
 package orientacaoObj;
 
 public class Ex01ClasseCliente {
-
+	private int age = 0;
+	private String name = "";
+	private String collor = "";		
+	private String user = "";
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCollor( ) {
+		return collor;
+	}
+	public void setCollor(String collor) {
+		this.collor = collor;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
 }
--- orientacaoObj/src/orientacaoObj/Ex01Main.java
+++ orientacaoObj/src/orientacaoObj/Ex01Main.java
@@ -1,5 +1,14 @@
 package orientacaoObj;
 
-public class Ex01Main {
-
+public class Ex01Main {	
+	public static void main(String[]agrs) {
+		Ex01ClasseCliente newClient = new Ex01ClasseCliente();
+		newClient.setName("Pedro");	
+		newClient.setUser("Drope");
+		newClient.setAge(19);
+		newClient.setCollor("Red");
+		
+		System.out.println("Nome: " + newClient.getName() + " // Idade: " + newClient.getAge() + " // Usuário: " 
+		+ newClient.getUser() + " // Cor: " + newClient.getCollor());
+	}
 }
